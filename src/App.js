4325e080d5a881d91084344b6eb38e98ffe15cd4
Ade <ade@Ades-MacBook-Pro.local>
2024-05-28 18:24:02 +0100

    new update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import { Mint, Stake, Social } from "./Pages";
+import { Mint, Stake, Social, Home } from "./Pages";
 import "react-toastify/dist/ReactToastify.css";
 import Wlchecker from "./Pages/Wlchecker";
 import { TwitterCallback } from '@ekaruz/react-social-auth';
@@ -11,13 +11,13 @@ const App = () => {
     <div className="App bg-neutral-900">
       <BrowserRouter>  
         <Routes>
-          <Route path="/" element={<Stake />} />
+          <Route path="/" element={<Home />} />
           <Route path="/social" element={<Social />} />
           <Route path="/wlchecker" element={<Wlchecker />} />
           <Route path="/stake" element={<Stake />} />
           <Route path="/r/:invite-code" element={<Stake />} />
           <Route path="/s/:invite-code" element={<Social />} />
-          <Route path="/*" element={<Mint />} />
+          {/* <Route path="/*" element={<Mint />} /> */}
           <Route path='/callback/twitter' element={<TwitterCallback/>} />
         </Routes>
       </BrowserRouter>
